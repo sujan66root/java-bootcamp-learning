6affab84abf2c2fc4d7d189137cf58bd6d6cf738
Sujan Gautam <sujan.rock45@gmail.com>
2024-10-15 22:00:23 -0400

    revision / solved first leetcode problem

---
+++ ArrayLeetcode/src/TwoSum.java
@@ -0,0 +1,31 @@
+// Leetcode 1 : Two Sum
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+//    second approach
+    public int[] twoSumUsingHashmap(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{i, map.get(complement)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
